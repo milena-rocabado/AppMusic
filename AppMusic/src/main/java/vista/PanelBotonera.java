4ddfcbb2b594798fbddc0f38aa3e2028b23a2ac8
oskarlopez4 <oskar@LAPTOP-8FCTHCR6>
2021-06-24 12:04:20 +0200

    Actualizando botonera

--- AppMusic/src/main/java/vista/PanelBotonera.java
+++ AppMusic/src/main/java/vista/PanelBotonera.java
@@ -2,9 +2,18 @@ package vista;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import modelo.Cancion;
+import modelo.ListaCanciones;
+import umu.tds.componente.CancionComponente;
 
 @SuppressWarnings("serial")
 public class PanelBotonera extends JPanel {
@@ -13,25 +22,53 @@ public class PanelBotonera extends JPanel {
 	private JButton playBtn;
 	private JButton pauseBtn;
 	private JButton nxtBtn;
+	private JTable table;
+	private List<Cancion> lista;
+
 	/**
 	 * Create the panel.
+	 * 
+	 * @param table
+	 * @param lista 
 	 */
 	public PanelBotonera() {
 		setBackground(new Color(240, 255, 255));
 		prevBtn = new JButton("prev");
 		prevBtn.setPreferredSize(new Dimension(61, 23));
 		this.add(prevBtn);
-		
+
 		playBtn = new JButton("play");
 		playBtn.setPreferredSize(new Dimension(61, 23));
+		playBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int num = table.getSelectedRow();
+				if (num < 0)
+					System.out.println("No hay columna seleccionada");
+				else {
+					System.out.println("tamaño lista"+ lista.size());
+					Cancion cancion =lista.get(num);
+					System.out.println("La cancion señalada es: "+cancion.getId()+cancion.getInterprete());
+					System.out.println("Esta es la cancion en la fila: " + num);
+					String titulo = (String) table.getValueAt(num, 1);
+					System.out.println("Este es el titulo a reproducir: " + titulo);
+				}
+
+			}
+		});
 		this.add(playBtn);
-		
+
 		pauseBtn = new JButton("pause");
 		this.add(pauseBtn);
-		
+
 		nxtBtn = new JButton("nxt");
 		nxtBtn.setPreferredSize(new Dimension(61, 23));
 		this.add(nxtBtn);
 	}
-
+	
+	
+	public void actualizarPanelBotonera(JTable table, List<Cancion> lista) {
+		this.table = table;
+		this.lista = lista;
+	}
 }
